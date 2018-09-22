eb0f0f925ed868783fb2c94070035817227acb1a
Arhis-Onlight <13.23.666.as@gmail.com>
2018-09-22 16:16:39 +0300

    Indecision App updated

--- app.js
+++ app.js
@@ -1,85 +1,164 @@
+'use strict';
+
 console.info("Developed by Arhis Onlight");
-const app = {
+var app = {
     title: 'Indecision app',
     subtitle: 'Put your life in the hands of a computer',
-    options: [], 
+    options: []
 };
-const nums = [66, 23, 54];
-const onFormSubmit = (e) => {
+var nums = [66, 23, 54];
+var onFormSubmit = function onFormSubmit(e) {
     e.preventDefault(); // Prevents default submitting and run the following code
-    
-    const option = e.target.elements.option.value;
-    const isMatch = (option.trim()).match(/^.{1,30}$/g);
-    if(option && isMatch) {
+
+    var option = e.target.elements.option.value;
+    var isMatch = option.trim().match(/^.{1,30}$/g);
+    if (option && isMatch) {
         app.options.push(option);
         e.target.elements.option.value = '';
-        render();    
+        render();
     }
 };
 
-const onRemoveAll = () => {
+var onRemoveAll = function onRemoveAll() {
     app.options.length = 0;
     render();
 };
 
-const onMakeDecision = () => {
-    const randomNumber = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNumber];
+var onMakeDecision = function onMakeDecision() {
+    var randomNumber = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNumber];
     alert(option);
 };
 
-const appRoot = document.getElementById('app');
-const render = () => {
-    const template = (
-        <div id="indecision">
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>} 
-            <p>{app.options.length > 0 ? 'Here is your options:' : 'no options'}</p> 
-            <button className="btn btn-primary" disabled={app.options.length === 0} onClick = {onMakeDecision}>What should I do?</button>&nbsp;            
-            <button type="button" className="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModalCenter">
-                Remove all option
-            </button>
-
-            {/*Modal*/}
-            <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalCenterTitle">Remove All Options</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div className="modal-body">
-                    <p>Are you sure? Changes won't be <span><strong>undone</strong></span>.</p>
-                    <p><strong>Remove all options?</strong></p>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-success" data-dismiss="modal">Close</button>
-                    <button type="button" data-dismiss="modal" onClick={onRemoveAll} className="btn btn-danger">Remove all</button>
-                </div>
-                </div>
-            </div>
-            </div>
-
-            <ol>
-                {
-                    
-                    app.options.map((option)=>{ 
-                        !app.id ? (app.id = 0, app.id++) : undefined; 
-                        app.id++;                      
-                        return <li key={app.id}>{option}</li>;
-                    })
-                }
-                {delete app.id} 
-            </ol> 
-            
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" placeholder="Option" />&nbsp;
-                <button className = "btn btn-sm btn-outline-success">Add option</button>
-            </form>       
-        </div>
-    );   
+var appRoot = document.getElementById('app');
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        { id: 'indecision' },
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here is your options:' : 'no options'
+        ),
+        React.createElement(
+            'button',
+            { className: 'btn btn-primary', disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
+        ),
+        '\xA0',
+        React.createElement(
+            'button',
+            { type: 'button', className: 'btn btn-outline-danger', 'data-toggle': 'modal', 'data-target': '#exampleModalCenter' },
+            'Remove all option'
+        ),
+        React.createElement(
+            'div',
+            { className: 'modal fade', id: 'exampleModalCenter', tabIndex: '-1', role: 'dialog', 'aria-labelledby': 'exampleModalCenterTitle', 'aria-hidden': 'true' },
+            React.createElement(
+                'div',
+                { className: 'modal-dialog modal-dialog-centered', role: 'document' },
+                React.createElement(
+                    'div',
+                    { className: 'modal-content' },
+                    React.createElement(
+                        'div',
+                        { className: 'modal-header' },
+                        React.createElement(
+                            'h5',
+                            { className: 'modal-title', id: 'exampleModalCenterTitle' },
+                            'Remove All Options'
+                        ),
+                        React.createElement(
+                            'button',
+                            { type: 'button', className: 'close', 'data-dismiss': 'modal', 'aria-label': 'Close' },
+                            React.createElement(
+                                'span',
+                                { 'aria-hidden': 'true' },
+                                '\xD7'
+                            )
+                        )
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'modal-body' },
+                        React.createElement(
+                            'p',
+                            null,
+                            'Are you sure? Changes won\'t be ',
+                            React.createElement(
+                                'span',
+                                null,
+                                React.createElement(
+                                    'strong',
+                                    null,
+                                    'undone'
+                                )
+                            ),
+                            '.'
+                        ),
+                        React.createElement(
+                            'p',
+                            null,
+                            React.createElement(
+                                'strong',
+                                null,
+                                'Remove all options?'
+                            )
+                        )
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'modal-footer' },
+                        React.createElement(
+                            'button',
+                            { type: 'button', className: 'btn btn-success', 'data-dismiss': 'modal' },
+                            'Close'
+                        ),
+                        React.createElement(
+                            'button',
+                            { type: 'button', 'data-dismiss': 'modal', onClick: onRemoveAll, className: 'btn btn-danger' },
+                            'Remove all'
+                        )
+                    )
+                )
+            )
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                !app.id ? (app.id = 0, app.id++) : undefined;
+                app.id++;
+                return React.createElement(
+                    'li',
+                    { key: app.id },
+                    option
+                );
+            }),
+            delete app.id
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option', placeholder: 'Option' }),
+            '\xA0',
+            React.createElement(
+                'button',
+                { className: 'btn btn-sm btn-outline-success' },
+                'Add option'
+            )
+        )
+    );
 
     ReactDOM.render(template, appRoot);
 };
